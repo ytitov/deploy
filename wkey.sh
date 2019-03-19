74d86728380afeedf1ef935fcbb61a6ff7ac070d
Yuri Titov <ytitov@gmail.com>
2019-03-18 20:26:38 -0500

    add rsync to server

--- wkey.sh
+++ wkey.sh
@@ -1,12 +1,28 @@
-sshpath=$HOME/.ssh/$1
-shift
-restargs=$@
-#echo "running eval `ssh-agent -s`"
-echo "EXAMPLE: './wkey.sh id_key_2 git push git@github.com:ParseSoftware/docker-wordpress-base.git brewbids:brewbids -f'"
-commds="ssh-add $sshpath; $restargs"
-echo "running command: ssh-agent bash -c $commds"
-#eval `ssh-agent -s`
-ssh-agent bash -c "$commds"
+#!/bin/bash
+source info_funcs.sh
+warn_text "adding functions wkey, rsync_to_server"
+function wkey {
+  sshpath=$HOME/.ssh/$1
+  warn_text "Contents of $HOME/.ssh: "
+  warn_text "$(ls $HOME/.ssh/*.pub)"
+  shift
+  restargs=$@
+  #echo "running eval `ssh-agent -s`"
+  red_text "Usage:"
+  warn_text "EXAMPLE: './wkey.sh id_key_2 git push git@github.com:ParseSoftware/docker-wordpress-base.git brewbids:brewbids -f'"
+  commds="ssh-add $sshpath; $restargs"
+  red_text "Running command: $restargs"
+  #echo "running command: ssh-agent bash -c $commds"
+  #eval `ssh-agent -s`
+  ssh-agent bash -c "$commds"
+}
+
+function rsync_to_server {
+  echo "rsync to main server, first arg is folder or file no slashes, and second is the ip, given: $1, $2"
+  warn_text "rsync_to_server usage: <source folder> <ip address> <name of user>"
+  rsync -avz $1 $3@$2:/root
+}
+
 #ssh-agent bash -c 'ssh-add /yourkey; git clone git@github.com:user/project.git'
 ## example command that works:
 ## ssh-agent bash -c 'ssh-add /home/deploy/.ssh/id_key_2; git push git@github.com:ParseSoftware/docker-wordpress-base.git brewbids:brewbids -f'
