4c23b72edb8913af7e4d63b4f280281685afd485
Yuri Titov <ytitov@gmail.com>
2019-03-18 20:31:30 -0500

    create a key

--- wkey.sh
+++ wkey.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source info_funcs.sh
-warn_text "adding functions wkey, rsync_to_server"
+warn_text "adding functions wkey, rsync_to_server, create_ssh_key"
 function wkey {
   sshpath=$HOME/.ssh/$1
   warn_text "Contents of $HOME/.ssh: "
@@ -21,6 +21,11 @@ function rsync_to_server {
   echo "rsync to main server, first arg is folder or file no slashes, and second is the ip, given: $1, $2"
   warn_text "rsync_to_server usage: <source folder> <ip address> <name of user>"
   rsync -avz $1 $3@$2:/root
+}
+
+function create_ssh_key {
+  warn_text "create a key, with name of key as the only argument"
+  ssh-keygen -t ecdsa -b 521 -N "" -f ~/.ssh/$1
 }
 
 #ssh-agent bash -c 'ssh-add /yourkey; git clone git@github.com:user/project.git'
