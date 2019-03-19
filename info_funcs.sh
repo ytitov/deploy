8b6b850976ae237d2bb2aedcbeca21f19659fcea
Yuri Titov <ytitov@gmail.com>
2019-03-18 20:37:03 -0500

    update

--- info_funcs.sh
+++ info_funcs.sh
@@ -19,6 +19,34 @@ function warn_text {
 function list_item {
         color $WHITE " * $1"
 }
+
+warn_text "adding functions wkey, rsync_to_server, create_ssh_key"
+function wkey {
+  sshpath=$HOME/.ssh/$1
+  warn_text "Contents of $HOME/.ssh: "
+  warn_text "$(ls $HOME/.ssh/*.pub)"
+  shift
+  restargs=$@
+  #echo "running eval `ssh-agent -s`"
+  red_text "Usage:"
+  warn_text "EXAMPLE: './wkey.sh id_key_2 git push git@github.com:ParseSoftware/docker-wordpress-base.git brewbids:brewbids -f'"
+  commds="ssh-add $sshpath; $restargs"
+  red_text "Running command: $restargs"
+  #echo "running command: ssh-agent bash -c $commds"
+  #eval `ssh-agent -s`
+  ssh-agent bash -c "$commds"
+}
+
+function rsync_to_server {
+  echo "rsync to main server, first arg is folder or file no slashes, and second is the ip, given: $1, $2"
+  warn_text "rsync_to_server usage: <source folder> <ip address> <name of user>"
+  rsync -avz $1 $3@$2:/root
+}
+
+function create_ssh_key {
+  warn_text "create a key, with name of key as the only argument"
+  ssh-keygen -t ecdsa -b 521 -N "" -f ~/.ssh/$1
+}
 ###echo ""
 ###red_text " -----------------------ssh help: -------------------------------------- "
 ###list_item "https://help.github.com/en/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent"
